f072d01a48a2358c3f9d1a5e6ca55ecce9c51ce6
Guilherme Stiebler <guilhermemst@gmail.com>
2017-03-08 18:47:49 -0300

    Fix the canvas test. The important line: self.view.fov_buffer.value = (1.0, 1.0)

--- src/odemis/gui/test/comp_canvas_test.py
+++ src/odemis/gui/test/comp_canvas_test.py
@@ -540,14 +540,17 @@ class TestDblMicroscopeCanvas(test.GuiTestCase):
         self.assertEqual(mpp, self.view.mpp.value)
         self.view.show_crosshair.value = False
 
+        init_pos = (200.5 * mpp, 199.5 * mpp)
+        #init_pos = (0.0, 0.0)
+
         FILENAME = u"test" + tiff.EXTENSIONS[0]
-        w = 400
-        h = 256
+        w = 512
+        h = 250
         size = (w, h, 3)
         dtype = numpy.uint8
         md = {
             model.MD_PIXEL_SIZE: (mpp, mpp),
-            model.MD_POS: (200.5 * mpp, 199.5 * mpp),
+            model.MD_POS: init_pos,
             model.MD_DIMS: 'YXC'
         }
         arr = model.DataArray(numpy.zeros((h, w, 3), dtype="uint8"))
@@ -570,24 +573,27 @@ class TestDblMicroscopeCanvas(test.GuiTestCase):
         # 200, 200 => outside of the im1
         # (+0.5, -0.5) to make it really in the center of the pixel
         im2.metadata[model.MD_PIXEL_SIZE] = (mpp, mpp)
-        im2.metadata[model.MD_POS] = (200.5 * mpp, 199.5 * mpp)
+        im2.metadata[model.MD_POS] = init_pos
         im2.metadata[model.MD_DIMS] = "YXC"
         stream2 = RGBStream("s2", im2)
 
-        self.view.addStream(stream2)
         self.view.addStream(stream1)
+        test.gui_loop(0.1)
+        self.view.addStream(stream2)
+        self.view.fov_buffer.value = (1.0, 1.0)
+        self.view.mpp.value = mpp * 1.1
 
-        self.canvas.fit_to_content()
         # reset the mpp of the view, as it's automatically set to the first  image
-        test.gui_loop(3)
+        test.gui_loop(0.5)
 
         result_im = get_image_from_buffer(self.canvas)
+        #result_im.SaveFile('/home/gstiebler/Projetos/Delmic/tmp1.bmp', wx.BITMAP_TYPE_BMP)
         px2 = get_rgb(result_im, result_im.Width // 2, result_im.Height // 2)
-        # center pixel, 1/3 green, 2/3 blue
-        self.assertEqual(px2, (0, 76, 179))
+        # center pixel, 2/3 green, 1/3 blue. The green image is the largest image
+        self.assertEqual(px2, (0, 179, 76))
         px2 = get_rgb(result_im, result_im.Width // 2 - 30, result_im.Height // 2 - 30)
-        # background of the images, 1/3 green, 2/3 red
-        self.assertEqual(px2, (179, 76, 0))
+        # background of the images, 2/3 green, 1/3 red
+        self.assertEqual(px2, (76, 179, 0))
 
         self.view.mpp.value = mpp
 
@@ -597,7 +603,7 @@ class TestDblMicroscopeCanvas(test.GuiTestCase):
         # merge the images
         ratio = 0.5
         self.view.merge_ratio.value = ratio
-        # self.assertEqual(ratio, self.view.merge_ratio.value)
+        self.assertEqual(ratio, self.view.merge_ratio.value)
 
         test.gui_loop(0.5)
         # it's supposed to update in less than 0.5s
@@ -607,31 +613,49 @@ class TestDblMicroscopeCanvas(test.GuiTestCase):
         px = get_rgb(result_im, result_im.Width // 2, result_im.Height // 2)
         # center pixel, now pointing to the background of the larger squares
         # half red, half green
-        self.assertEqual(px, (127, 128, 0))
+        self.assertEqual(px, (128, 127, 0))
 
         # copy the buffer into a nice image here
         result_im = get_image_from_buffer(self.canvas)
 
+        #result_im.SaveFile('/home/gstiebler/Projetos/Delmic/tmp2.bmp', wx.BITMAP_TYPE_BMP)
+
         px1 = get_rgb(result_im, result_im.Width // 2 + shift[0], result_im.Height // 2 + shift[1])
-        self.assertEqual(px1, (0, 128, 127))  # Ratio is at 0.5, so 255 becomes 128
+        self.assertEqual(px1, (0, 127, 128))  # Ratio is at 0.5, so 255 becomes 128
 
         px2 = get_rgb(result_im,
                       result_im.Width // 2 + 200 + shift[0],
                       result_im.Height // 2 - 200 + shift[1])
         self.assertEqual(px2, (0, 0, 0))
 
-        # remove first picture
+        self.assertAlmostEqual(1e-05, self.view.mpp.value)
+        numpy.testing.assert_almost_equal([0.001375,  0.002625], self.view.view_pos.value)
+        self.canvas.fit_view_to_content(recenter=True)
+        test.gui_loop(0.5)
+        self.assertAlmostEqual(1.28e-05, self.view.mpp.value)
+        # after fitting, the center of the view should be the center of the image
+        numpy.testing.assert_almost_equal(init_pos, self.view.view_pos.value)
+        # remove green picture
+        result_im = get_image_from_buffer(self.canvas)
+        #result_im.SaveFile('/home/gstiebler/Projetos/Delmic/tmp3.bmp', wx.BITMAP_TYPE_BMP)
         self.view.removeStream(stream1)
-        test.gui_loop()
+        test.gui_loop(0.5)
+        # copy the buffer into a nice image here
+        result_im = get_image_from_buffer(self.canvas)
+        #result_im.SaveFile('/home/gstiebler/Projetos/Delmic/tmp4.bmp', wx.BITMAP_TYPE_BMP)
+        self.canvas.fit_view_to_content(recenter=True)
         test.gui_loop(0.5)
 
         result_im = get_image_from_buffer(self.canvas)
+
+        #result_im.SaveFile('/home/gstiebler/Projetos/Delmic/tmp5.bmp', wx.BITMAP_TYPE_BMP)
         # center of the translated red square with blue square on the center
         # pixel must be completely blue
         px2 = get_rgb(result_im,
                       result_im.Width // 2 + shift[0],
                       result_im.Height // 2 + shift[1])
-        self.assertEqual(px2, (0, 0, 255))
+        # the center is red
+        self.assertEqual(px2, (255, 0, 0))
 
         self.canvas.fit_to_content()
 
