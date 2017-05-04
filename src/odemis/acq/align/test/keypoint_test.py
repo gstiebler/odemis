96baac2e4f03c5d4d3064f1e1018c17edeb99c9a
Guilherme Stiebler <guilhermemst@gmail.com>
2017-05-04 20:07:10 -0300

    removing the test, as the function get_img_transformation_md now needs the images

--- src/odemis/acq/align/test/keypoint_test.py
+++ src/odemis/acq/align/test/keypoint_test.py
@@ -132,24 +132,6 @@ class TestKeypoint(unittest.TestCase):
         self.assertAlmostEqual(179.37792038862722, tmetadata[model.MD_POS][0])
         self.assertAlmostEqual(-154.15625938805169, tmetadata[model.MD_POS][1])
 
-    def test_get_img_transformation_md(self):
-        rot = 0.2
-        scale_x = 0.9
-        scale_y = 0.8
-        shear = -0.15
-        ps_mat = numpy.matrix([[scale_x, 0], [0, scale_y]])
-        rcos, rsin = math.cos(rot), math.sin(rot)
-        rot_mat = numpy.matrix([[rcos, -rsin], [rsin, rcos]])
-        shear_mat = numpy.matrix([[1, 0], [-shear, 1]])
-        tmat = rot_mat * shear_mat * ps_mat
-
-        tmat33 = numpy.zeros((3, 3), dtype=numpy.float)
-        tmat33[0:2, 0:2] = tmat
-        transf_md = get_img_transformation_md(tmat33)
-        self.assertAlmostEqual(rot, transf_md[model.MD_ROTATION])
-        self.assertEqual((scale_x, scale_y), transf_md[model.MD_PIXEL_SIZE])
-        self.assertAlmostEqual(shear, transf_md[model.MD_SHEAR])
-
     def test_synthetic_images(self):
         image = numpy.zeros((1000, 1000, 4), dtype=numpy.uint8)
         surface = cairo.ImageSurface.create_for_data(image, cairo.FORMAT_ARGB32, 1000, 1000)
